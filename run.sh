bce5ef9fcd679a0d22b5901ea7d2e40ce1c0cbed
Dima Korolev <dima@current.ci>
2025-03-21 08:44:38 -0700

    s/`md5sum`/`md5`/g.

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-R1=$(g++ -std=c++17 xoshiro256.cc && ./a.out | md5sum | cut -f1 -d' ')
+R1=$(g++ -std=c++17 xoshiro256.cc && ./a.out | md5 | cut -f1 -d' ')
 if [ "$R1" != "c9d58312358dbc51cd8eb553872a3f4d" ] ; then
   echo "C++ fail."
   exit 1
 fi
 echo "C++ OK."
 
-R2=$(rustc xoshiro256.rs && ./xoshiro256 | md5sum | cut -f1 -d' ')
+R2=$(rustc xoshiro256.rs && ./xoshiro256 | md5 | cut -f1 -d' ')
 if [ "$R2" != "c9d58312358dbc51cd8eb553872a3f4d" ] ; then
   echo "Rust fail."
   exit 1
