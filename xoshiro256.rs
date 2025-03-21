e24dac45245c1f846fc1ecad0cd8793768154ad8
Dima Korolev <dima@current.ci>
2025-03-21 08:22:21 -0700

    The code.

---
+++ xoshiro256.rs
@@ -0,0 +1,34 @@
+use std::num::Wrapping;
+
+pub struct Xoshiro256 {
+  state: [Wrapping<u64>; 4],
+}
+
+impl Xoshiro256 {
+  pub fn new(seed: [u64; 4]) -> Self { Self { state: seed.map(Wrapping) } }
+
+  pub fn next(&mut self) -> u64 {
+    let result = (self.state[0] + self.state[3]).0;
+    let t = (self.state[1] << 17).0;
+    self.state[2] ^= self.state[0];
+    self.state[3] ^= self.state[1];
+    self.state[1] ^= self.state[2];
+    self.state[0] ^= self.state[3];
+    self.state[2] ^= Wrapping(t);
+    self.state[3] = Wrapping(self.state[3].0.rotate_left(45));
+    result
+  }
+}
+
+fn main() {
+  let seed = [
+    0x123456789abcdef0,
+    0xfedcba9876543210,
+    0xdeadbeefcafebabe,
+    0x0badc0ffee0ddf00d
+  ];
+  let mut rng = Xoshiro256::new(seed);
+  for _ in 0..50 {
+    println!("{}", rng.next());
+  }
+}
